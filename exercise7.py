5d0d80ddd733c83a2740aba55666abdf3b56c49b
xeroeight <47375091+xeroeight@users.noreply.github.com>
2019-12-12 09:09:10 -0800

    Create exercise7.py

---
+++ exercise7.py
@@ -0,0 +1,8 @@
+# Exercise 7
+class Car:
+
+    def __init__(self,year, make, model):
+        self.year = 2019-year
+        self.make = make
+        self.model = model
+Car(1994, 'make', 'Toyota').year
